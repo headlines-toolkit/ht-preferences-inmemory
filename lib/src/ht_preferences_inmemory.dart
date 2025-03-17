e9154c8392ccffcb6d45363f7e0e377ae3baf670
fulleni <fulleni@hotmail.com>
2025-03-17 15:50:42 +0100

    feat: initial commit
    - Added project structure
    - Created basic inmemory impl
    - Added issue templates
    - Added pull request template

---
+++ lib/src/ht_preferences_inmemory.dart
@@ -0,0 +1,7 @@
+/// {@template ht_preferences_inmemory}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtPreferencesInmemory {
+  /// {@macro ht_preferences_inmemory}
+  const HtPreferencesInmemory();
+}
---
+++ test/src/ht_preferences_inmemory_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_preferences_inmemory/ht_preferences_inmemory.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtPreferencesInmemory', () {
+    test('can be instantiated', () {
+      expect(HtPreferencesInmemory(), isNotNull);
+    });
+  });
+}
