ba96657345bd6518dd984e642deee0cc124192a4
fulleni <fulleni@hotmail.com>
2025-03-17 16:18:50 +0100

    feat: implement in-memory preferences client
    - Implemented HtPreferencesInMemory
    - Added methods for preferences
    - Added exceptions for not found

--- lib/ht_preferences_inmemory.dart
+++ lib/ht_preferences_inmemory.dart
@@ -1,4 +1,6 @@
-/// A Very Good Project created by Very Good CLI.
+/// Inmemory implementation of the `HtPreferencesClient` interface.
+/// It's primarily intended for development purposes.
 library;
 
+export 'package:ht_preferences_client/ht_preferences_client.dart';
 export 'src/ht_preferences_inmemory.dart';
--- lib/src/ht_preferences_inmemory.dart
+++ lib/src/ht_preferences_inmemory.dart
@@ -1,7 +1,115 @@
+//ignore_for_file: lines_longer_than_80_chars
+
+import 'package:ht_preferences_client/ht_preferences_client.dart';
+
 /// {@template ht_preferences_inmemory}
-/// A Very Good Project created by Very Good CLI.
+/// An in-memory implementation of [HtPreferencesClient].
 /// {@endtemplate}
-class HtPreferencesInmemory {
+class HtPreferencesInMemory implements HtPreferencesClient {
   /// {@macro ht_preferences_inmemory}
-  const HtPreferencesInmemory();
+  HtPreferencesInMemory();
+
+  String _language = 'en';
+  ThemeMode _theme = ThemeMode.system;
+  final List<String> _favoriteSources = [];
+  final List<String> _favoriteCategories = [];
+  final List<String> _favoriteEventCountries = [];
+
+  @override
+  Future<String> getLanguage() async {
+    return _language;
+  }
+
+  @override
+  Future<void> setLanguage(String language) async {
+    if (language.isEmpty || language.length != 2) {
+      throw InvalidLanguageException(language);
+    }
+    _language = language;
+  }
+
+  @override
+  Future<ThemeMode> getTheme() async {
+    return _theme;
+  }
+
+  @override
+  Future<void> setTheme(ThemeMode theme) async {
+    _theme = theme;
+  }
+
+  @override
+  Future<List<String>> getFavoriteSources() async {
+    return _favoriteSources;
+  }
+
+  @override
+  Future<void> setFavoriteSources(List<String> sources) async {
+    _favoriteSources
+      ..clear()
+      ..addAll(sources);
+  }
+
+  @override
+  Future<void> addFavoriteSource(String source) async {
+    _favoriteSources.add(source);
+  }
+
+  @override
+  Future<void> removeFavoriteSource(String source) async {
+    if (!_favoriteSources.contains(source)) {
+      throw SourceNotFoundException(source);
+    }
+    _favoriteSources.remove(source);
+  }
+
+  @override
+  Future<List<String>> getFavoriteCategories() async {
+    return _favoriteCategories;
+  }
+
+  @override
+  Future<void> setFavoriteCategories(List<String> categories) async {
+    _favoriteCategories
+      ..clear()
+      ..addAll(categories);
+  }
+
+  @override
+  Future<void> addFavoriteCategory(String category) async {
+    _favoriteCategories.add(category);
+  }
+
+  @override
+  Future<void> removeFavoriteCategory(String category) async {
+    if (!_favoriteCategories.contains(category)) {
+      throw CategoryNotFoundException(category);
+    }
+    _favoriteCategories.remove(category);
+  }
+
+  @override
+  Future<List<String>> getFavoriteEventCountries() async {
+    return _favoriteEventCountries;
+  }
+
+  @override
+  Future<void> setFavoriteEventCountries(List<String> countries) async {
+    _favoriteEventCountries
+      ..clear()
+      ..addAll(countries);
+  }
+
+  @override
+  Future<void> addFavoriteEventCountry(String country) async {
+    _favoriteEventCountries.add(country);
+  }
+
+  @override
+  Future<void> removeFavoriteEventCountry(String country) async {
+    if (!_favoriteEventCountries.contains(country)) {
+      throw CountryNotFoundException(country);
+    }
+    _favoriteEventCountries.remove(country);
+  }
 }
