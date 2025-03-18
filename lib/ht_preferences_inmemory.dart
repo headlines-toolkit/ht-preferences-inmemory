2bf2c740065f2c436e8dbb0f063e3342bbd19eb8
fulleni <fulleni@hotmail.com>
2025-03-18 07:07:53 +0100

    refactor: Re-export types from client package
    - Re-export necessary types
    - For better API consistency

--- lib/ht_preferences_inmemory.dart
+++ lib/ht_preferences_inmemory.dart
@@ -2,5 +2,20 @@
 /// It's primarily intended for development purposes.
 library;
 
-export 'package:ht_preferences_client/ht_preferences_client.dart';
+// Re-export the necessary types from the ht_preferences_client interface.
+export 'package:ht_preferences_client/ht_preferences_client.dart'
+    show
+        AppFontSize,
+        CategoryNotFoundException,
+        ContentDensity,
+        CountryNotFoundException,
+        FeedTileLayout,
+        HeadlineArticleTextSize,
+        HeadlineNotFoundException,
+        HtPreferencesClient,
+        InvalidLanguageException,
+        PreferencesStorageException,
+        SourceNotFoundException,
+        ThemeMode;
+
 export 'src/ht_preferences_inmemory.dart';
